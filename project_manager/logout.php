0d5341ffff00ad683cb886932851181de6afdca3
Lily-126 <lily.le@myokanagan.bc.ca>
2025-11-27 20:48:39 -0700

    Update logout.php

---
+++ project_manager/logout.php
@@ -0,0 +1,33 @@
+<?php
+/** 
+ * Logout Handler
+ * 
+ * This script handles user logout by destroying the session and all session data.
+ * It demontrates prper session management and security best practices for logout.
+ */
+
+session_start();
+
+// Unset all session variables
+$_SESSION = [];
+
+// Destroy the session cookie
+if (isset($_COOKIE[session_name()])){
+    setcookie(session_name(), '', time() - 3600, '/');
+}
+
+// Destroy the session
+
+session_destroy();
+
+header("Location: login.php");
+exit;
+?>
+
+<?php
+if(isLoggedIn()){
+    header('Location: dashboard.php');
+    exit;
+}
+
+$error = '';
