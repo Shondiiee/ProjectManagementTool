9d7eab68209172a76f8a012583e3ae9e7ec3dfb3
Lily-126 <lily.le@myokanagan.bc.ca>
2025-11-28 14:14:03 -0700

    Update login.php

--- project_manager/login.php
+++ project_manager/login.php
@@ -54,3 +54,15 @@ if(empty($username) || empty($password)){
         <label for="username">Username</label>
         <input type="text" id="username" name="username" required value="<?php echo h($_POST['username']?? ''); ?>">
     </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" required>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Login</button>
+</form>
+<p class="text-center">Don't have an account? <a href="register.php">Register here</a></p>
+</div>
+</div>
+</body>
+</html>
